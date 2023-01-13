c1537cff064f94bc8c335d929a4e995042adb17e
rasmusParkering <121871712+rasmusParkering@users.noreply.github.com>
2023-01-13 14:13:04 +0100

    before messeing with heights

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,9 @@ const Footer = (props) => {
 			<h4 className="footer__text">
 				Husk altid at starte din parkering i app eller ved brug af QR-kode.
 			</h4>
-			<img className="footer__image" src={Oparko} alt="Oparko - Parkeringssystemer til erhverv"/>
+			<div>
+				<img className="footer__image" src={Oparko} alt="Oparko - Parkeringssystemer til erhverv"/>
+			</div>
 		</footer>
 	);
 };
